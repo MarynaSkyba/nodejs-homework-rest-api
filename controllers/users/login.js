f12f0ae0fe71a8bc4f940fb13fe469eb977bf62c
Maryna <marynaskyba@gmail.com>
2021-12-20 12:02:24 +0100

    add resendEmail

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -8,8 +8,8 @@ const {SECRET_KEY} =process.env;
 const login = async (req, res)=> {
     const {email, password} = req.body;
     const user = await User.findOne({email})
-    if(!user || !user.comparePassword(password)){
-        throw new Unauthorized("Email or password is wrong")
+    if(!user || !user.verify || !user.comparePassword(password)){
+        throw new Unauthorized("Email or password is wrong or your email not verify")
     }
     // if(!user){
     //     throw new Unauthorized(`${email} is wrong`)
--- routes/api/users.js
+++ routes/api/users.js
@@ -12,5 +12,8 @@ router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", auth, ctrlWrapper(ctrl.logOut))
 // router.patch("/:contactId", auth, validation(subscriptionJoiSchema), ctrlWrapper(ctrl.updateSubscription))
 router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar))
+router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail))
+router.post("/verify", ctrlWrapper(ctrl.resendEmail))
+
 
 module.exports = router; 
